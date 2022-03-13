d345a448b799947bb3d86a06b4ab487e681ebb21
Arthur Campolina <arthurcampolina@hotmal.com>
2022-03-13 18:58:03 -0300

    First request

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,14 +1,20 @@
+import axios from "axios";
+import { BASE_URL } from "utils/requests";
 import MovieCard from "../../components/MovieCard";
 import Pagination from "../../components/Pagination";
 
 function Listing() {
+  // FORMA ERRADA
+  axios.get(`${BASE_URL}/movies?size=12&page=0`).then(response => {
+    console.log(response.data);
+  });
+
   return (
     <>
       <Pagination />
 
       <div className="container">
         <div className="row">
-
           <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
             <MovieCard />
           </div>
@@ -24,10 +30,8 @@ function Listing() {
           <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
             <MovieCard />
           </div>
-                    
         </div>
       </div>
-
     </>
   );
 }
