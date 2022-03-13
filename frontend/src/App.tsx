00585db5e720a456fd748149ce07566e47aa4a84
Arthur Campolina <arthurcampolina@hotmal.com>
2022-03-13 11:34:44 -0300

    Movie card

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Listing from "./components/pages/Listing";
-import Form from "./components/pages/Form";
+import Listing from "./pages/Listing";
+import Form from "./pages/Form";
 import Navbar from "./components/Navbar";
 
 function App() {
